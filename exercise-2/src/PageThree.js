a610e654e17927ac7c5bfb0019a9e64af9342fec
Jenna Han <jenna3@uw.edu>
2016-11-15 09:18:54 -0800

    class

---
+++ exercise-2/src/PageThree.js
@@ -0,0 +1,14 @@
+// Page of quotes to show
+import React from 'react';
+
+// PageOne Component
+var PageThree = React.createClass({
+	// Render a <Quote> element for each element in the state
+	render() {
+		return (
+			<div>Page threee!</div>
+		);
+	}
+});
+
+export default PageThree;
